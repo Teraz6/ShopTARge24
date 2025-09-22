de313ac7dd08816d85f1d6f7ef82a7622fe0fdbc
Teraz6 <retenk@gmail.com>
2025-09-22 22:37:10 +0300

    Update Program.cs

--- WebShop/WebShop/Program.cs
+++ WebShop/WebShop/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
 builder.Services.AddDbContext<WebShopContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDbContext<KindergardenDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("KindergardenConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
