28def72af8eb54d0d1d9f7d6b47ffb07e10cb201
Teraz6 <retenk@gmail.com>
2025-09-22 22:35:56 +0300

    Add ViewModels

---
+++ WebShop/WebShop/Models/Kindergardens/KindergardenCreateUpdateViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebShop.Models.Kindergardens
+{
+    public class KindergardenCreateUpdateViewModel
+    {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string KindergardenName { get; set; }
+        public string TeacherName { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ WebShop/WebShop/Models/Kindergardens/KindergardenIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebShop.Models.Kindergardens
+{
+    public class KindergardenIndexViewModel
+    {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string KindergardenName { get; set; }
+        public string TeacherName { get; set; }
+    }
+}
