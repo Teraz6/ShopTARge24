e44abf930b00fe2fc06e1e5bebe1912ac066d646
Teraz6 <retenk@gmail.com>
2025-09-22 22:36:24 +0300

    Add Services and DTO

---
+++ WebShop/WebShop.ApplicationServices/Services/KindergardenServices.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShop.Core.Domain;
+using WebShop.Core.Dto;
+using WebShop.Core.ServiceInterface;
+using WebShop.Data;
+
+namespace WebShop.ApplicationServices.Services
+{
+    public class KindergardenServices : IKindergardenServices
+    {
+        private readonly WebShopContext _context;
+
+        public KindergardenServices(WebShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Kindergarden> Create(KindergardenDto dto)
+        {
+            Kindergarden kindergarden = new()
+            {
+                Id = Guid.NewGuid(),
+                GroupName = dto.GroupName,
+                ChildrenCount = dto.ChildrenCount,
+                KindergardenName = dto.KindergardenName,
+                TeacherName = dto.TeacherName,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            _context.Kindergardens.Add(kindergarden);
+            await _context.SaveChangesAsync();
+            return kindergarden;
+        }
+
+        public async Task<Kindergarden> Update(KindergardenDto dto)
+        {
+            var domain = await _context.Kindergardens.FirstOrDefaultAsync(x => x.Id == dto.Id);
+
+            if (domain == null)
+            {
+                return null;
+            }
+
+            domain.GroupName = dto.GroupName;
+            domain.ChildrenCount = dto.ChildrenCount;
+            domain.KindergardenName = dto.KindergardenName;
+            domain.TeacherName = dto.TeacherName;
+            domain.UpdatedAt = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+            return domain;
+        }
+
+        public async Task<Kindergarden> Delete(Guid id)
+        {
+            var kindergarden = await _context.Kindergardens.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (kindergarden == null)
+            {
+                return null;
+            }
+
+            _context.Kindergardens.Remove(kindergarden);
+            await _context.SaveChangesAsync();
+
+            return kindergarden;
+        }
+
+        public async Task<Kindergarden> DetailAsync(Guid id)
+        {
+            return await _context.Kindergardens.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
---
+++ WebShop/WebShop.Core/ServiceInterface/IKindergardenSevices.cs
@@ -0,0 +1,13 @@
+﻿using WebShop.Core.Dto;
+using WebShop.Core.Domain;
+
+namespace WebShop.Core.ServiceInterface
+{
+    public interface IKindergardenServices
+    {
+        Task<Kindergarden> Create(KindergardenDto dto);
+        Task<Kindergarden> Update(KindergardenDto dto);
+        Task<Kindergarden> Delete(Guid id);
+        Task<Kindergarden> DetailAsync(Guid id);
+    }
+}
