64f9921f379c100ec7cff9b1f8eb344d3a52cb9f
Teraz6 <retenk@gmail.com>
2025-09-22 21:26:52 +0300

    Create KindergardenContext.cs

---
+++ WebShop/WebShop.Data/KindergardenContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace WebShop.Data
+{
+    public class KindergardenDbContext : DbContext
+    {
+        public KindergardenDbContext(DbContextOptions<KindergardenDbContext> options) : base(options) { }
+
+        public DbSet<Kindergarden> Kindergardens { get; set; }
+    }
+}
