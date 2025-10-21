4dc05ba12229cf02177ff4acf0f144e3bc7b84bd
Teraz6 <retenk@gmail.com>
2025-10-21 16:40:03 +0300

    Add ChuckNorris page
    Unfinished

---
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/ChuckNorrisServices.cs
@@ -0,0 +1,51 @@
+﻿using Nancy.Json;
+using ShopTARge24.Core.Dto.WeatherWebClientDto;
+using ShopTARge24.Core.Dto;
+using System.Net;
+using System.Text;
+
+namespace ShopTARge24.ApplicationServices.Services
+{
+    public class ChuckNorrisServices
+    {
+
+        public async Task<ChuckNorrisDto> ChuckNorrisResult(ChuckNorrisDto dto)
+        {
+
+            var baseUrl = "https://api.chucknorris.io/jokes/random";
+
+            using (var httpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={apiKey}&q={dto.CityName}"),
+                    Content = new StringContent("", Encoding.UTF8, "application/json"),
+                };
+
+                httpClient.BaseAddress = new Uri(baseUrl);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //127964
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+
+            }
+
+            using (var clientChuckNorris = new HttpClient())
+            {
+                var httpResponseChuckNorris = await clientChuckNorris.GetAsync(ChuckNorrisResponse);
+                string jsonChuckNorris = await httpResponseChuckNorris.Content.ReadAsStringAsync();
+
+                ChuckNorrisDto ChuckNorrisDto =
+                    JsonSerializer.Deserialize<ChuckNorrisDto>(jsonChuckNorris);
+
+                dto.IconUrl = ChuckNorrisDto.IconUrl;
+                dto.Id = ChuckNorrisDto.Id;
+                dto.Url = ChuckNorrisDto.Url;
+                dto.Value = ChuckNorrisDto.Value;
+            }
+
+            return dto;
+        }
+    }
+}
---
+++ ShopTARge24/ShopTARge24/Controllers/ChuckNorrisController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Core.Dto;
+using ShopTARge24.Core.ServiceInterface;
+using ShopTARge24.Models.ChuckNorris;
+
+namespace ShopTARge24.Controllers
+{
+    public class ChuckNorrisController : Controller
+    {
+        private readonly IChuckNorrisServices _chuckNorrisServices;
+
+        public ChuckNorrisController
+            (
+                IChuckNorrisServices chuckNorrisServices
+            )
+        {
+            _chuckNorrisServices = chuckNorrisServices;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetNorrisFacts(ChuckNorrisViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction();
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Id(string Id)
+        {
+            ChuckNorrisDto dto = new();
+            dto.Id = Id;
+
+            _chuckNorrisServices.ChuckNorrisResult(dto);
+            ChuckNorrisViewModel vm = new();
+
+            vm.IconUrl = dto.IconUrl;
+            vm.Id = dto.Id;
+            vm.Url = dto.Url;
+            vm.Value = dto.Value;
+
+            return View(vm);
+        }
+    }
+}
