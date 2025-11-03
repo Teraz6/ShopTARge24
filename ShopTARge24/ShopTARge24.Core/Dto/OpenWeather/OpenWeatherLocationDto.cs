ef8af80eb52c53db93ee2f5f4546d581ac063053
Teraz6 <retenk@gmail.com>
2025-11-03 19:52:00 +0200

    Add Openweather dtos

---
+++ ShopTARge24/ShopTARge24.Core/Dto/OpenWeather/OpenWeatherLocationDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace ShopTARge24.Core.Dto.OpenWeatherDto
+{
+    public class OpenWeatherLocationDto
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("lat")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("lon")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+    }
+}
