6add6825a9f2c3bf89195bf8baa648955fa76506
Teraz6 <retenk@gmail.com>
2025-11-03 20:14:05 +0200

    Add services

---
+++ ShopTARge24/ShopTARge24.Core/ServiceInterface/IOpenWeatherServices.cs
@@ -0,0 +1,9 @@
+﻿using ShopTARge24.Core.Dto.OpenWeatherDto;
+
+namespace ShopTARge24.Core.ServiceInterface
+{
+    public interface IOpenWeatherServices
+    {
+        Task<OpenWeatherResultDto> OpenWeatherResult(OpenWeatherResultDto dto);
+    }
+}
