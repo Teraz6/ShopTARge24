a7eda5b9b56e95d21a1a4170b158cca1c098c679
Teraz6 <retenk@gmail.com>
2025-11-25 11:25:36 +0200

    Add EmailServices and Dto

--- ShopTARge24/ShopTARge24/Controllers/RealEstateController.cs
+++ ShopTARge24/ShopTARge24/Controllers/RealEstateController.cs
@@ -126,6 +126,15 @@ namespace ShopTARge24.Controllers
                 Location = vm.Location,
                 CreatedAt = vm.CreatedAt,
                 ModifiedAt = vm.ModifiedAt,
+                Files = vm.Files,
+                Image = vm.Image
+                    .Select(x => new FileToDatabaseDto
+                    {
+                        Id = x.ImageId,
+                        ImageData = x.ImageData,
+                        ImageTitle = x.ImageTitle,
+                        RealEstateId = x.RealEstateId
+                    }).ToArray()
             };
 
             var result = await _realEstateServices.Update(dto);
